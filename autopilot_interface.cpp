270cfafbd86a5e8238073dbbee24eb309acd220e
Frank Zhang <zhangh94@gmail.com>
2016-05-05 09:23:34 -0700

    Modifications

--- autopilot_interface.cpp
+++ autopilot_interface.cpp
@@ -495,7 +495,7 @@ enable_offboard_control()
     // Encode message
     mavlink_message_t message;
     mavlink_msg_cpslo_msg_encode(system_id, companion_id, &message, &com);
-    
+    printf("\n MessageID: %u\n", message.msgid);
     // Send the message 
     int len;
     len = serial_port->write_message(message);
